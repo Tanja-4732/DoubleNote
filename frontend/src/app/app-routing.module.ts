83a855f1ced8b476111c5103ed18cba22435dc38
Bernd-L <git@bernd.pw>
2020-03-10 11:30:39 +0100

    npm run watch

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { MainComponent } from "./components/main/main.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  // Main component
+  { path: "/main", pathMatch: "full", component: MainComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
