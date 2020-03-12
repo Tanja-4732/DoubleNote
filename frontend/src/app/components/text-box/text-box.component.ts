7003ee83df4fe7441b005f0265f2119dc90fb827
Bernd-L <git@bernd.pw>
2020-03-12 16:43:52 +0100

    New components

---
+++ frontend/src/app/components/text-box/text-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-text-box',
+  templateUrl: './text-box.component.html',
+  styleUrls: ['./text-box.component.scss']
+})
+export class TextBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
