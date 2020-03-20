69a7038ba01f7dc6883835c459596f80c4f80eef
Bernd-L <git@bernd.pw>
2020-03-20 04:44:32 +0100

    Begun WYSIWYG editor implementation

--- frontend/src/app/components/text-box/text-box.component.ts
+++ frontend/src/app/components/text-box/text-box.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-text-box',
-  templateUrl: './text-box.component.html',
-  styleUrls: ['./text-box.component.scss']
+  selector: "app-text-box",
+  templateUrl: "./text-box.component.html",
+  styleUrls: ["./text-box.component.scss"]
 })
 export class TextBoxComponent implements OnInit {
+  // @ViewChild("wysiwyg")
+  // wysiwyg: Element;
 
-  constructor() { }
+  state = "both";
+
+  constructor() {}
 
   ngOnInit(): void {
+    // this.wysiwyg.contentEditable = true;
   }
 
+  cycleModes() {
+    switch (this.state) {
+      case "both":
+        this.state = "markdown";
+        break;
+      case "markdown":
+        this.state = "wysiwyg";
+        break;
+      case "wysiwyg":
+        this.state = "both";
+        break;
+    }
+  }
 }
