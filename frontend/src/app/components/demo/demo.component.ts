12599604300bc7c38cf2c18df4412f48d07abc08
Bernd-L <git@bernd.pw>
2020-03-20 19:45:45 +0100

    One-way WebRTC works!

--- frontend/src/app/components/demo/demo.component.ts
+++ frontend/src/app/components/demo/demo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core";
+import { v4 } from "uuid";
+import Peer, { DataConnection } from "peerjs";
 
 @Component({
   selector: "app-demo",
@@ -6,10 +8,61 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./demo.component.scss"]
 })
 export class DemoComponent implements OnInit {
+  peer: Peer;
+
+  /**
+   * The ID of the (other) peer
+   */
+  peerId: string;
+
+  /**
+   * ~my~ OUR message (soviet anthem starts playing)
+   */
+  private _peerMessage: string;
+
+  get peerMessage(): string {
+    return this._peerMessage;
+  }
+
+  set peerMessage(message: string) {
+    this._peerMessage = message;
+    this.connection.send(message);
+  }
+
+  connection: DataConnection;
+
   constructor() {}
 
   height = 100;
   width = 200;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // Create a peer for myself
+    this.peer = new Peer(v4());
+
+    // Listen for incoming connections
+    this.peer.on("connection", conn => {
+      this.connection = conn;
+      this.connection.on("data", (data: string) => {
+        console.log(data);
+
+        this._peerMessage = data;
+      });
+      // conn.on("open", () => {
+      //   conn.send("hello!");
+      // });
+    });
+  }
+
+  /**
+   * Initiates a connection
+   */
+  connectToPeer() {
+    this.connection = this.peer.connect(this.peerId);
+
+    // When the outgoing connection is established
+    this.connection.on("open", () => {
+      console.log("Peer connected");
+    });
+  }
 }
