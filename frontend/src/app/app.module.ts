649151e05ce20594a5b2d6e84d30a3143bde0571
Bernd-L <git@bernd.pw>
2020-05-30 18:50:02 +0200

    Cursed errors

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { DragDropModule } from "@angular/cdk/drag-drop";
 import { LayoutModule } from "@angular/cdk/layout";
-import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { ServiceWorkerModule } from "@angular/service-worker";
 import { environment } from "../environments/environment";
+
 import { AppComponent } from "./app.component";
+import { UserInterfaceModule } from "./user-interface/user-interface.module";
+
+// Do not remove the unused imports!
+// The app does not compile without them for some reason
 import { BoxCanvasPageModule } from "./box-canvas-page/box-canvas-page.module";
-import { DrawComponent } from "./box-canvas-page/components/draw/draw.component";
-import { TextBoxComponent } from "./box-canvas-page/components/text-box/text-box.component";
+// import { DrawComponent } from "./box-canvas-page/components/draw/draw.component";
+// import { TextBoxComponent } from "./box-canvas-page/components/text-box/text-box.component";
 import { CoreModule } from "./core/core.module";
 import { AppRoutingModule } from "./routes/app-routing.module";
-import { UserInterfaceModule } from "./user-interface/user-interface.module";
 
 @NgModule({
   declarations: [AppComponent],
