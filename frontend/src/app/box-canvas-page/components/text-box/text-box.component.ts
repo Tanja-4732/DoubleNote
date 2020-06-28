6bb56cb11cb610b5b9a658145cdee69d2b4885eb
Bernd-L <git@bernd.pw>
2020-06-28 18:14:37 +0200

    Fix the WYSIWYG root element

--- frontend/src/app/box-canvas-page/components/text-box/text-box.component.ts
+++ frontend/src/app/box-canvas-page/components/text-box/text-box.component.ts
@@ -282,7 +282,7 @@ export class TextBoxComponent implements OnInit, OnDestroy {
     log(mutationsList);
 
     const mdom = this.engine.parseDOM(
-      Array.from(this.wysiwygEditor.nativeElement.children)
+      Array.from(this.wysiwygEditor.nativeElement.children[0].children)
     );
 
     this.mb.dispatchMessage({
