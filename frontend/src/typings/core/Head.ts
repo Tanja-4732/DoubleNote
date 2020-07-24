9204b3db3c37e8972b7194048e589e8f232dc914
Bernd-L <git@bernd.pw>
2020-07-24 18:50:36 +0200

    VCS Tag typings

--- frontend/src/typings/core/Head.ts
+++ frontend/src/typings/core/Head.ts
@@ -1,20 +1,41 @@
 import { Commit } from "./Commit";
 
-export type Head = CommitHead | BranchHead | TagHead;
+export type Head = CommitHead | BranchHead;
 
 interface BaseHead {
-  type: "commit" | "branch" | "tag";
+  type: "commit" | "branch";
+
+  /**
+   * Whether the HEAD is in a detached state
+   *
+   * This is the case if the HEAD doesn't directly point to a branch.
+   */
+  detached: boolean;
 }
 
 export interface CommitHead extends BaseHead {
   type: "commit";
+
+  detached: true;
+
+  /**
+   * The commit to which the detached HEAD points to
+   */
   commit: Commit;
 }
 
 export interface BranchHead extends BaseHead {
-  tag: "branch";
-}
+  type: "branch";
 
-export interface TagHead extends BaseHead {
-  type: "tag";
+  detached: false;
+
+  /**
+   * The name of the branch to which the HEAD points to
+   */
+  name: string;
+
+  /**
+   * The commit behind the selected branch
+   */
+  commit: Commit;
 }
