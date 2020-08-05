ecb89658cbf0f370acd39535e34ac219be6f1606
Bernd-L <git@bernd.pw>
2020-08-05 21:13:06 +0200

    CLI plans

---
+++ package/src/commands/commands.ts
@@ -0,0 +1,14 @@
+// TODO parse CLI commands
+
+/**
+ * - Modes
+ *   - HTTP
+ *   - HTTPS
+ *   - Both
+ *   - Redirect to HTTPS
+ * - Ports
+ *   - HTTP (default to 80)
+ *   - HTTPS (default to 443)
+ *
+ * Use something like Commander.js
+ */
