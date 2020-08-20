f46ee253ad50d8f8f86d2f8eeed4d0a3122359a7
Bernd-L <git@bernd.pw>
2020-08-20 16:13:25 +0200

    Better goals for the SessionService

--- frontend/src/app/services/session/session.service.ts
+++ frontend/src/app/services/session/session.service.ts
@@ -10,6 +10,8 @@ import { Injectable } from "@angular/core";
  * - Provide an interface for listing, storing and retrieving all notebooks
  *   - Refactor the application to use the session service instead of calling the VCS services directly
  *   - Implement APIs for both BCP and SBP components and services
+ *   - Listen for save messages from the MessageBus
+ *   - Download and upload notebooks to and from peers to establish shared sessions
  * - Outside code should be mostly agnostic to the session owner
  *   - Allow the pre-existing code to interface with local and remote notebooks
  */
