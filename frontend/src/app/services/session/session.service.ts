abdfa3136dba560ffc4b496f4ba1af4a5a0129b0
Bernd-L <git@bernd.pw>
2020-08-20 14:00:34 +0200

    Goals for the SessionService

--- frontend/src/app/services/session/session.service.ts
+++ frontend/src/app/services/session/session.service.ts
@@ -1,5 +1,26 @@
 import { Injectable } from "@angular/core";
 
+/**
+ * # Session Service
+ *
+ * Used to manage sessions and their states
+ *
+ * ## Goals
+ *
+ * - Provide an interface for listing, storing and retrieving all notebooks
+ *   - Refactor the application to use the session service instead of calling the VCS services directly
+ *   - Implement APIs for both BCP and SBP components and services
+ * - Outside code should be mostly agnostic to the session owner
+ *   - Allow the pre-existing code to interface with local and remote notebooks
+ * - Manage connections using contacts
+ *   - Joining remote sessions provided an invitation
+ *   - Inviting others to collaboration in the local session
+ * - The MessageBusService
+ *   - Move the contacts here from the MessageBusService
+ *   - Move establishing connections here from the MessageBusService
+ *   - Maybe keep the dispatch of messages in the MessageBusService...
+ *   - ...or maybe remove the MessageBusService (and use this instead)
+ */
 @Injectable({
   providedIn: "root",
 })
--- frontend/src/typings/core/Session.ts
+++ frontend/src/typings/core/Session.ts
@@ -1,1 +1,33 @@
-export interface Session {}
+export interface Session {
+  /**
+   * The UUID of the session host/owner
+   */
+  hostUuid: string;
+
+  /**
+   * If the user is the host of the session
+   */
+  amHost: boolean;
+
+  /**
+   * The notebooks to share
+   */
+  shares: NotebookShare[];
+}
+
+export interface NotebookShare {
+  /**
+   * The type of the shared notebook
+   */
+  type: "BCP" | "SBP";
+
+  /**
+   * The UUID of the shared notebook
+   */
+  uuid: string;
+
+  /**
+   * If write access is enabled
+   */
+  writable: boolean;
+}
